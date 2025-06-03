5135aeef8b154696be5c33499f0bfcee63a2e5a3
Haikal79 <haikalanugerah79@gmail.com>
2025-06-03 15:24:13 +0700

    Create App.jsx

---
+++ frontend/App.jsx
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2025 John Doe
+ */
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>KantinGo - Pesan Makanan Online</h1>
+    </div>
+  );
+};
+
+export default App;
