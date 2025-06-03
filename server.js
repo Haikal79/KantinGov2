3910d44a331647f1624fdff6c05ddc65d2a7848d
Haikal79 <haikalanugerah79@gmail.com>
2025-06-03 15:35:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- * Copyright (c) 2025 John Doe
- */
+/* 
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+Copyright (c) 2025 John Doe 
+*/
 
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Selamat datang di KantinGo!');
